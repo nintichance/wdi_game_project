11d89104ee90e2f887a1f7d89386aa5e1b689ed2
nintichance <mintidance@gmail.com>
2017-12-12 00:09:44 -0500

    I made another js file called 'pseudocode.js' which is where I will write any of my pseudocode and any test code for my main.js file.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
     alert("The DOM is ready! Yay!")
-    $('.container > .q1').on('click', function(){
+    $('.container > .q1').on('click', function () {
         let testAnswer = prompt("I am an adulty thing that has to do with stocks. I rhyme with iPhones")
-        if (testAnswer === "dow jones"){
+        if (testAnswer === "dow jones") {
             alert("you are correct")
             $(this).css('background-color', 'green')
+
         } else {
             alert("sorry, you are not adult enough")
             $(this).css('background-color', 'red')
         }
-        })
-    
+    })
+    $('.container > .q1').data('point-value', '100')
+    let pointString = $('.container > .q1').data('point-value')
+    console.log(pointString)
 })
---
+++ js/pseudocode.js
@@ -0,0 +1,16 @@
+ //UPDATE SCORE
+    //if answer is correct
+    //score += value of question
+//DEDUCT FROM SCORE
+    //if answer is incorrect
+    //score -= value of question
+
+//I need the value of the question's points
+//Score is collected in a <p> in the div with 
+//class of "score"
+//add the value of the questions points to the 
+//value of score
+
+//POINT VALUE
+$('.container > .q1').data('point-value', '100')
+let pointString = $('.container > .q1').data('point-value')
