0f87dd08963341f2650ce7488ea7158639f102fc
nintichance <mintidance@gmail.com>
2017-12-11 23:23:40 -0500

    I made a propmpt that asks the user a question. If answered correctly, the user is alerted that they are correct and the div turns green.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     alert("The DOM is ready! Yay!")
     $('.container > .q1').on('click', function(){
-        alert("ya clicked me!")
+        let testAnswer = prompt("I am an adulty thing that has to do with stocks. I rhyme with iPhones")
+        if (testAnswer === "dow jones"){
+            alert("you are correct")
+            $(this).css('background-color', 'green')
+        }
         })
+    
 })
