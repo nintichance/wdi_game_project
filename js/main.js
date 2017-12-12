ac33833be1542a4f60056d606e509059c384645a
nintichance <mintidance@gmail.com>
2017-12-12 11:59:16 -0500

    Tested array of answers in pseudocode.js to see if I can push an object's name's value into another array.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+
+
 $(document).ready(function () {
     alert("The DOM is ready! Yay!")
+    // let hoverOnDiv = $('.container > .q1').on('hover', function (){
+    //     $('.container > .q1').css('background-color', 'orange')
+    // })
     let userAnswers = $('.container > .q1').on('click', function () {
         let testAnswer = prompt("I am an adulty thing that has to do with stocks. I rhyme with iPhones")
         if (testAnswer === "dow jones") {
@@ -10,23 +15,6 @@ $(document).ready(function () {
             alert("sorry, you are not adult enough")
             $(this).css('background-color', 'red')
         }
-        $('.container > .q1').data('point-value', '100')
-        let pointString = $('.container > .q1').data('point-value')
-        let pointNum = parseInt(pointString)
-        $('.scoreContainer > .score').data('points', '0')
-        let scoreString = $('.scoreContainer > .score').data('points')
-        let scoreNum = parseInt(scoreString)
-        console.log(scoreNum)
-
-        if (testAnswer === "dow jones"){
-            let scoreNumDisplay = scoreNum + pointNum
-            } else {
-            let scoreNumDisplay = scoreNum - pointNum
-            }
-            let scoreView = $('<p>')
-            $('.scoreContainer > .score').remove('.scoreContainer > .default')
-            $('.scoreContainer > .score').append(scoreView)
-            scoreView.text(`Score: ${userAnswers.scoreNumDisplay}`)    
     })
-    
 })
+
--- js/pseudocode.js
+++ js/pseudocode.js
@@ -1,17 +1,28 @@
-//  //UPDATE SCORE
-//     //if answer is correct
-//     //score += value of question
-// //DEDUCT FROM SCORE
-//     //if answer is incorrect
-//     //score -= value of question
+// //  //UPDATE SCORE
+// //     //if answer is correct
+// //     //score += value of question
+// // //DEDUCT FROM SCORE
+// //     //if answer is incorrect
+// //     //score -= value of question
 
-// //I need the value of the question's points
-// //Score is collected in a <p> in the div with 
-// //class of "score"
-// //add the value of the questions points to the 
-// //value of score
+// // //I need the value of the question's points
+// // //Score is collected in a <p> in the div with 
+// // //class of "score"
+// // //add the value of the questions points to the 
+// // //value of score
 
 // //POINT VALUE
+// $('.container > .q1').on('click', function () {
+//     let testAnswer = prompt("I am an adulty thing that has to do with stocks. I rhyme with iPhones")
+//     if (testAnswer === "dow jones") {
+//         alert("you are correct")
+//         $(this).css('background-color', 'green')
+
+//     } else {
+//         alert("sorry, you are not adult enough")
+//         $(this).css('background-color', 'red')
+//     }
+
 // $('.container > .q1').data('point-value', '100')
 // let pointString = $('.container > .q1').data('point-value')
 // let pointNum = parseInt(pointString)
@@ -20,19 +31,139 @@
 // let scoreNum = parseInt(scoreString)
 // console.log(scoreNum)
 
+// let scoreNumDisplay;
 // //ADDING POINTS
-// if (testVariable.testAnswer === "dow jones"){
-// let scoreNumDisplay = scoreNum + pointNum
+// if (testAnswer === "dow jones"){
+//  scoreNumDisplay = scoreNum + pointNum
 // } else {
-// let scoreNumDisplay = scoreNum - pointNum
+//  scoreNumDisplay = scoreNum - pointNum
 // }
 
 // //SCORE THE USER SEES
 // let scoreView = $('<p>')
 // $('.scoreContainer > .score').append(scoreView)
 // scoreView.text(`Score: ${scoreNumDisplay}`)
+// })
 
 
-// //ONLY ADD POINTS IF ANSWER IS RIGHT
+// // //ONLY ADD POINTS IF ANSWER IS RIGHT
+let answersArray = []
+const answerQuestion = [
+    {
+    answer:'When I need to iron my pants with a nice firm crease ready to impress all of my business peers, I use this ironing thingy.',
+    question: 'What is that stuff that comes in a spray bottle. I think its called starch.'
+    }, 
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    },
+    {
+     answer: 'answer',
+     question: 'question'
+    }
+]
+for (let i = 0; i < answerQuestion.length; i++){
+    answersArray.push(answerQuestion[i].answer)
+}
 
 
+
+
+
+// // 
+
+//ASSIGN QUESTIONS TO EACH POINT DIV
+
+//questions to ask
+//Is there a way to specifically assign a question to a particular DOM element?
+//I
+
+//ASSIGN DATA VALUES TO EACH POINT DIV
